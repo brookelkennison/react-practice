4c99918454bc20199deb211bedd07c8f5cba30a0
Brooke Kennison <brookelkennison@gmail.com>
2019-12-18 11:58:29 -0600

    loading component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { fetchPopularRepos } from '../utils/api';
 import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa';
 import Card from './Card';
+import Loading from './Loading';
 
 function LangugesNav ({selected, onUpdateLanguge}) {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -41,7 +42,7 @@ function ReposGrid ({ repos }) {
                 return (
                     <li key={html_url}>
                         <Card
-                            header={`#{index + 1}`}
+                            header={`#${index + 1}`}
                             avatar={avatar_url}
                             href={html_url}
                             name={login}
@@ -132,7 +133,7 @@ export default class Popular extends React.Component {
                     onUpdateLanguge={this.updateLanguage}
                 />
 
-                {this.isLoading() && <p>LOADING</p>}
+                {this.isLoading() && <Loading text='Fetching Repos' />}
 
                 {error && <p className='center-text error'>{error}</p>}
 
